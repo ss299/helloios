4b689193093cd7230596706ef85669486424cf85
s299 <ss299@uw.edu>
2021-04-06 14:42:34 -0700

    ios app

---
+++ Hello/Hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello
+//
+//  Created by Saurav Sawansukha on 4/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Cougars Suck!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
